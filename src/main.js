46eef4b602152e536e5ad685357594fd9d1e887d
xiaokai2hao <lkrexe@qq.com>
2019-05-03 20:10:14 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import elementUi from 'element-ui'
+import normalize from './assets/normalize.css'
 
 Vue.config.productionTip = false
+Vue.use(elementUi);
+Vue.use(normalize);
 
 /* eslint-disable no-new */
 new Vue({
